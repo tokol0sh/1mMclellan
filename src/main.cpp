1ba5e301cca32482636578f4f9b01bd8c05343a6
Byron <12byron34@gmail.com>
2017-09-01 22:06:52 +1200

    Times and coordinates now look correct. LST works too. Very inefficient method of calculating the delta's. Will look into using the precomputed context and the quick transform functions. As it is works plenty fast tho. Next step is to get the pointing corrections working.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@
 #include <iostream>
 #include <thread>
 
-void reprd(char* s, double ra, double dc, double hr = 0, double dr = 0);
+
+void reprd(char* s, double ra, double dc, double hr = 0, double dr = 0, double LST = 0);
 
 
 
@@ -18,31 +19,37 @@ int main()
 
 	typedef duration<int, ratio_multiply<hours::period, ratio<24> >::type> days;
 
-	system_clock::time_point previous_time_point;
+	std::chrono::time_point<std::chrono::high_resolution_clock> previous_time_point_high_res;
+	std::chrono::time_point<std::chrono::high_resolution_clock> current_time_point_high_res;
 	system_clock::time_point current_time_point;
 	system_clock::time_point current_time_ms;
 	tm utc_tm;
 	time_t current_time_t;
+	milliseconds msec = duration_cast<milliseconds>(current_time_point - current_time_ms);
+	double delta_t;
+
+	double previous_RA, previous_DEC, previous_HA, LST;
+	double delta_RA, delta_DEC, delta_HA;
+	double rate_RA, rate_DEC, rate_HA;
 
 	iauASTROM astrom;
-	iauLDBODY b[3];
 	double latitude, longitude, height, barometric_pressure, temperature, relative_humidity, wavelength, utc1, utc2, tai1, tai2,
 		tt1, tt2, ut11,ut12, xp, yp, dut1, dx, dy, rc, dc, pr, pd, px, rv,
 		eo, ri, di, rca, dca, ra, da, aot, zot, hot, dot, rot,
-		aob, zob, hob, dob, rob,
-		pvh[2][3], pvb[2][3], r[3][3], x, y, s;
+		aob, zob, hob, dob, rob, aob_next, zob_next, hob_next, dob_next, rob_next,
+		pvh[2][3], x, y, s;
 
-	double previous_RA, previous_DEC, previous_HA;
-	double delta_RA, delta_DEC, delta_HA;
-	double rate_RA, rate_DEC, rate_HA;
+	/* Minimum cos(alt) and sin(alt) for refraction purposes */
+	const double CELMIN = 1e-6;
+	const double SELMIN = 0.05;
+
 
 
 	current_time_point = system_clock::now();
 	current_time_t = system_clock::to_time_t(current_time_point);
 	utc_tm = *gmtime(&current_time_t);
 	current_time_ms = system_clock::from_time_t(current_time_t);
-	milliseconds msec = duration_cast<milliseconds>(current_time_point - current_time_ms);
-	milliseconds delta_t;
+
 
 
 	/* Site longitude, latitude (radians) and height above the geoid (m). */
@@ -58,6 +65,7 @@ int main()
 	/* Effective color (microns). */
 	wavelength = 0.55;
 
+
 	// These can be obtained form IERS Bulletin B: ftp://hpiers.obspm.fr/iers/bul/bulb_new/bulletinb.dat
 	/* EOPs:  polar motion in radians, UT1-UTC in seconds. */
 	xp = 209.842e-3 * DAS2R;
@@ -68,6 +76,7 @@ int main()
 	dx = 0 * DAS2R;
 	dy = 0 * DAS2R;
 
+
 	/* Time  */
 	iauDtf2d("UTC", (2017), utc_tm.tm_mon + 1, utc_tm.tm_mday, utc_tm.tm_hour, utc_tm.tm_min, (utc_tm.tm_sec + msec.count()/1000.0), &utc1, &utc2);
 	iauUtctai(utc1, utc2, &tai1, &tai2);
@@ -75,10 +84,15 @@ int main()
 	iauUtcut1(utc1, utc2, dut1, &ut11, &ut12);
 
 
+
+
 	/* Star ICRS RA,Dec (radians). */
-	iauTf2a(' ', 14, 26, 14.15, &rc);
-	iauAf2a('-', 43, 24,28.5, &dc);
+	iauTf2a(' ', 17, 37, 19.15, &rc);
+	iauAf2a('-', 42, 59,52.2, &dc);
 	reprd("ICRS, epoch J2000.0:", rc, dc);
+
+
+
 
 	/* Proper motion: RA/Dec derivatives, epoch J2000.0. */
 	pr = atan2(-0 * DAS2R, cos(dc));
@@ -88,72 +102,111 @@ int main()
 	px = 0.0;
 	rv = 0.0;
 
-	/* ICRS to CIRS (geocentric observer). */
-	iauAtci13(rc, dc, pr, pd, px, rv, tt1, tt2, &ri, &di, &eo);
-	reprd("catalog -> CIRS:", ri, di);
 
-	/* CIRS to ICRS (astrometric). */
-	iauAtic13(ri, di, tt1, tt2, &rca, &dca, &eo);
-	reprd("CIRS -> astrometric:", rca, dca);
+	// Calculate star independent astrometry paramaters
+	iauApco13(utc1, utc2, dut1, longitude, latitude, relative_humidity, xp, yp, barometric_pressure, temperature, relative_humidity, wavelength, &astrom, &eo);
 
-	/* ICRS (astrometric) to CIRS (geocentric observer). */
-	iauAtci13(rca, dca, 0.0, 0.0, 0.0, 0.0, tt1, tt2, &ri, &di, &eo);
-	reprd("astrometric -> CIRS:", ri, di);
+	// Transform from ICRS to CIRS
+	iauAtciq(rc, dc, pr, pd, px, rv, &astrom, &ri, &di);
+	reprd("ICRS -> CIRS:", ri, di);
 
-	/* Apparent place. */
+	/* CIRS to Apparent place. */
 	ra = iauAnp(ri - eo);
 	da = di;
-	reprd("geocentric apparent:", ra, da);
+	reprd("Classical Apparent place:", ra, da);
 
 	/* CIRS to topocentric. */
 	if (iauAtio13(ri, di, utc1, utc2, dut1, longitude, latitude, height, xp, yp,
 		0.0, 0.0, 0.0, 0.0,
 		&aot, &zot, &hot, &dot, &rot)) return -1;
+	reprd("CIRS -> topocentric:", astrom.eral - rot, dot);
+
+	// CIRS to Observed
+	iauAtioq(ri, di, &astrom, &aob, &zob, &hob, &dob, &rob);
+	reprd("CIRS -> observed:", astrom.eral - rob, dob);
+
+
+
+
+/*  
+// Proper motion: RA/Dec derivatives, epoch J2000.0. 
+	pr = atan2(-0 * DAS2R, cos(dc));
+	pd = 0 * DAS2R;
+
+	///* Parallax (arcsec) and recession speed (km/s). 
+	px = 0.0;
+	rv = 0.0;
+
+	// ICRS to CIRS (geocentric observer). 
+	iauAtci13(rc, dc, pr, pd, px, rv, tt1, tt2, &ri, &di, &eo);
+	reprd("catalog -> CIRS:", ri, di);
+
+	//CIRS to ICRS (astrometric). 
+	iauAtic13(ri, di, tt1, tt2, &rca, &dca, &eo);
+	reprd("CIRS -> astrometric:", rca, dca);
+
+	//ICRS (astrometric) to CIRS (geocentric observer). 
+	iauAtci13(rca, dca, 0.0, 0.0, 0.0, 0.0, tt1, tt2, &ri, &di, &eo);
+	reprd("astrometric -> CIRS:", ri, di);
+
+	///Apparent place. 
+	ra = iauAnp(ri - eo);
+	da = di;
+	reprd("geocentric apparent:", ra, da);
+
+	// CIRS to topocentric. 
+	if (iauAtio13(ri, di, utc1, utc2, dut1, longitude, latitude, height, xp, yp,
+		0.0, 0.0, 0.0, 0.0,
+		&aot, &zot, &hot, &dot, &rot)) return -1;
 	reprd("CIRS -> topocentric:", rot, dot);
 
-	/* CIRS to observed. */
+	// CIRS to observed. 
 	if (iauAtio13(ri, di, utc1, utc2, dut1, longitude, latitude, height, xp, yp,
 		barometric_pressure, temperature, relative_humidity, wavelength,
 		&aob, &zob, &hob, &dob, &rob)) return -1;
 	reprd("CIRS -> observed:", rob, dob);
 
-	/* ICRS to observed. */
+	*/
 
-
-	/* UTC date. */
+	///UTC date. 
 	while (1) {
-		previous_time_point = current_time_point;
 		current_time_point = system_clock::now();
 
-		delta_t = duration_cast<milliseconds>(current_time_point - previous_time_point);
+		delta_t = 50.0;
 
 		current_time_t = system_clock::to_time_t(current_time_point);
 		utc_tm = *gmtime(&current_time_t);
 		current_time_ms = system_clock::from_time_t(current_time_t);
 		milliseconds msec = duration_cast<milliseconds>(current_time_point - current_time_ms);
 
-		int test = 0;
-		test = (iauDtf2d("UTC", 1900 + utc_tm.tm_year, utc_tm.tm_mon+1, utc_tm.tm_mday, utc_tm.tm_hour, utc_tm.tm_min, (utc_tm.tm_sec + msec.count() / 1000.0), &utc1, &utc2));
-		//std::cout << utc_tm.tm_hour << ':' << utc_tm.tm_min << ':' << (utc_tm.tm_sec + msec.count() / 1000.0) << '\n' ;
-		/* TT date. */
-		if (iauUtctai(utc1, utc2, &tai1, &tai2)) return -1;
-		if (iauTaitt(tai1, tai2, &tt1, &tt2)) return -1;
-		previous_RA = rob;
-		previous_HA = hob;
-		previous_DEC = dob;
-		if (iauAtco13(rc, dc, pr, pd, px, rv, utc1, utc2, dut1,
-			longitude, latitude, height, xp, yp, barometric_pressure, temperature, relative_humidity, wavelength,
-			&aob, &zob, &hob, &dob, &rob, &eo)) return -1;
-		delta_RA = rob - previous_RA;
-		delta_DEC = dob - previous_DEC;
-		delta_HA = hob - previous_HA;
 
-		rate_RA = ((delta_RA * 1e9) / delta_t.count()) / 1e6;
-		rate_DEC = ((delta_DEC * 1e9) / delta_t.count()) / 1e6;
-		rate_HA = ((delta_HA * 1e9) / delta_t.count() ) / 1e6;
+		iauDtf2d("UTC", 1900 + utc_tm.tm_year, utc_tm.tm_mon + 1, utc_tm.tm_mday, utc_tm.tm_hour, utc_tm.tm_min, (utc_tm.tm_sec + msec.count() / 1000.0), &utc1, &utc2);
+		iauUtctai(utc1, utc2, &tai1, &tai2);
+		iauTaitt(tai1, tai2, &tt1, &tt2);
 
-		reprd("ICRS -> observed:", hob, dob, rate_HA, rate_DEC);
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		// Calculate star independent astrometry paramaters
+		iauApco13(utc1, utc2, dut1, longitude, latitude, relative_humidity, xp, yp, barometric_pressure, temperature, relative_humidity, wavelength, &astrom, &eo);
+		// CIRS to Observed
+		iauAtioq(ri, di, &astrom, &aob, &zob, &hob, &dob, &rob);
+
+		LST = eo - astrom.eral;
+
+		iauDtf2d("UTC", 1900 + utc_tm.tm_year, utc_tm.tm_mon + 1, utc_tm.tm_mday, utc_tm.tm_hour, utc_tm.tm_min, (delta_t + utc_tm.tm_sec + msec.count() / 1000.0), &utc1, &utc2);
+		iauUtctai(utc1, utc2, &tai1, &tai2);
+		iauTaitt(tai1, tai2, &tt1, &tt2);
+
+		// Calculate star independent astrometry paramaters
+		iauApco13(utc1, utc2, dut1, longitude, latitude, relative_humidity, xp, yp, barometric_pressure, temperature, relative_humidity, wavelength, &astrom, &eo);
+		// CIRS to Observed
+		iauAtioq(ri, di, &astrom, &aob_next, &zob_next, &hob_next, &dob_next, &rob_next);
+
+		rate_RA = (rob_next - rob) / delta_t;
+		rate_DEC = (dob_next - dob) / delta_t;
+		rate_HA = (hob_next - hob) / delta_t;
+
+
+		reprd("ICRS -> observed:", rob, dob, rate_RA, rate_HA, LST);
+		//std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
 	}
 	
@@ -161,7 +214,7 @@ int main()
 }
 
 
-void reprd(char* s, double ra, double dc, double hr , double dr )
+void reprd(char* s, double ra, double dc, double hr , double dr , double LST)
 {
 	char pm = '-';
 	int i[4];
@@ -173,6 +226,8 @@ void reprd(char* s, double ra, double dc, double hr , double dr )
 	iauA2af(7, hr, &pm, i);
 	printf(" %2.2d %2.2d %2.2d.%7.7d", i[0], i[1], i[2], i[3]);
 	iauA2af(7, dr, &pm, i);
+	printf(" %2.2d %2.2d %2.2d.%7.7d", i[0], i[1], i[2], i[3]);
+	iauA2tf(7, LST, &pm, i);
 	printf(" %2.2d %2.2d %2.2d.%7.7d\n", i[0], i[1], i[2], i[3]);
 }
 
